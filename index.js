f37faebd7304322e1f05f41fcb1b307622ae6919
Brock Rohloff <00agentb@gmail.com>
2018-01-27 04:54:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObject =  Object.assign({[key]: value}, object);
+  return newObject;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+}
